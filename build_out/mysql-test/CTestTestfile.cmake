61584b653d18d5b826dadcebfb77e8718435020a
xh <xuehao@shanzhen.com>
2021-12-15 13:17:46 +0800

    init commit

--- build_out/mysql-test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xh/workspaces/learning/mysql-5.5.62/mysql-test
-# Build directory: /Users/xh/workspaces/learning/mysql-5.5.62/mysql-test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_out/sql-bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xh/workspaces/learning/mysql-5.5.62/sql-bench
-# Build directory: /Users/xh/workspaces/learning/mysql-5.5.62/sql-bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
